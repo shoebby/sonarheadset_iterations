e7189b3f957d605f00e53bbaab2fa01a7927258e
shoebby <59800629+shoebby@users.noreply.github.com>
2020-01-12 18:03:39 +0100

    Update sonarHeadset_v3.ino

--- sonarHeadset_v3.ino
+++ sonarHeadset_v3.ino
@@ -1,13 +1,13 @@
-#include <NewTone.h> //deze is erin gehaald omdat de newPing library een conflict heeft met de normale tone() gerelateerde functies
-#include <NewPing.h> //voor de sonar
+#include <NewTone.h> //This library has been imported due to the newPing library conflicting with the normal tone() function
+#include <NewPing.h> //for the ultrasonic sensor
 
-NewPing sonar(10,11); //sonar
-int speaker = 8; //buzzer
+NewPing sonar(10,11); //ultrasonic sensor
+int speaker = 8; //speaker/buzzer
 
 void setup() {
   Serial.begin(9600);
-  pinMode(speaker, OUTPUT); //buzzer
-  pinMode(A0, INPUT); //druksensor
+  pinMode(speaker, OUTPUT); //speaker/buzzer
+  pinMode(A0, INPUT); //force-sensitive resistor
 }
 
 void loop() {
@@ -15,16 +15,16 @@ void loop() {
 
   int pressure = analogRead(A0);
   int distance = sonar.ping_cm();
-  int frequency = map(distance, 0, 100, 1500, 250); //geregistreerde afstand door sonar wordt omgezet naar een waarde die bruikbaar is als frequentie van een toon
-  int toneDelay = map(distance, 0, 100, 100, 1000); //zelfde maar dan voor de delay tussen de tonen
+  int frequency = map(distance, 0, 100, 1500, 250); //registered distance by ultrasonic sensor is translated to a frequency between 1500 and 250
+  int toneDelay = map(distance, 0, 100, 100, 1000); //same as above, but instead translated to a delay between 100 and 1000
   
-  Serial.print("Afstand=");
+  Serial.print("Distance=");
   Serial.println(distance);
   Serial.println(pressure);
 
   if (pressure > 300) {
 
-    if (distance == 0) { //wanneer de sonar niks oppikt komt er 0 uit als waarde, dus om eindeloos gepiep te voorkomen komt er geen toon wanneer de afstand 0 wordt geregistreerd
+    if (distance == 0) { //when the ultrasonic sensor registers a value of 0 no tone is being played, this is to prevent the device from beeping when it doesnt register anything
       noNewTone(speaker);
     } else {
       NewTone(speaker, frequency);
